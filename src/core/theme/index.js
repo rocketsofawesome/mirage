f5ba75fc19f1470d497d3f4100ef02bc74fb8e84
Shawn Mealey <mealeyst@gmail.com>
2018-12-06 17:37:54 -0500

    Removing PropType check on ThemeWrapper

--- src/core/theme/index.js
+++ src/core/theme/index.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { ThemeProvider } from 'styled-components'
 import theme from './theme'
 
 const ROATheme = (props) => {
   return (<ThemeProvider theme={theme} {...props} />)
-}
-
-ROATheme.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.object,
-    PropTypes.string
-  ]).isRequired
 }
 
 export default ROATheme
