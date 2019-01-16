e5c16303d7b705872f45bdb06b21674d86410549
Shawn Mealey <mealeyst@gmail.com>
2019-01-16 11:56:02 -0500

    Updating the build process YET AGAIN

--- rollup.config.js
+++ rollup.config.js
@@ -7,48 +7,67 @@ import replace from 'rollup-plugin-replace';
 import path from 'path';
 import fs from 'fs';
 
+const dependencies = Object.keys(require('./package.json').dependencies)
+console.log(dependencies)
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'umd',
-    name: 'Mirage',
-    globals: {
-      react: 'React',
-      'prop-types': 'PropTypes',
-      'styled-components': 'styled'
-    }
-  },
-  // All the used libs needs to be here
-  external: [
-    'react',
-    'prop-types',
-    'styled-components',
-  ],
-  plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify( 'production' )
-    }),
-    resolve(),
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    alias({
-      SRC: resolveApp('src'),  // Will check for ./bar.jsx and ./bar.js
-      resolve: ['.js', '/index.js'] ,
-    }),
-    commonjs({
-      include: 'node_modules/**',
-      exclude: ['node_modules/react', 'node_modules/react-dom'],
-      namedExports: {
-        // left-hand side can be an absolute path, a path
-        // relative to the current directory, or the name
-        // of a module in node_modules
-        'react-sizeme': [ 'withSize' ]
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/bundle.main.js',
+      format: 'umd',
+      name: 'Mirage',
+      globals: {
+        react: 'React',
+        'prop-types': 'PropTypes',
+        'styled-components': 'styled'
       }
-    })
-  ]
-}
+    },
+    // All the used libs needs to be here
+    external: dependencies,
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify( 'production' )
+      }),
+      resolve(),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      alias({
+        SRC: resolveApp('src'),  // Will check for ./bar.jsx and ./bar.js
+        resolve: ['.js', '/index.js'] ,
+      }),
+      commonjs({
+        include: 'node_modules/**',
+        exclude: ['node_modules/react', 'node_modules/react-dom'],
+        namedExports: {
+          // left-hand side can be an absolute path, a path
+          // relative to the current directory, or the name
+          // of a module in node_modules
+          'react-sizeme': [ 'withSize' ]
+        }
+      })
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/bundle.module.js',
+      format: 'cjs'
+    },
+    // All the used libs needs to be here
+    external: dependencies,
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      alias({
+        SRC: resolveApp('src'),  // Will check for ./bar.jsx and ./bar.js
+        resolve: ['.js', '/index.js'] ,
+      }),
+    ]
+  }
+]
