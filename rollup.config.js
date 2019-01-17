1421ef09560e15b615a3de6a387b5f878c6001e4
Shawn Mealey <mealeyst@gmail.com>
2019-01-17 10:42:09 -0500

    Adding the build back, re-adding the YellowCircleChev

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,70 @@
-
 // node-resolve will resolve all the node dependencies
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import alias from 'rollup-plugin-alias';
+import replace from 'rollup-plugin-replace';
 import path from 'path';
 import fs from 'fs';
 
+const dependencies = Object.keys(require('./package.json').dependencies)
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/bundle.main.js',
+      format: 'umd',
+      name: 'Mirage',
+      globals: {
+        react: 'React',
+        'prop-types': 'PropTypes',
+        'styled-components': 'styled'
+      }
+    },
+    // All the used libs needs to be here
+    external: dependencies,
+    plugins: [
+
+      resolve(),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      alias({
+        SRC: resolveApp('src'),  // Will check for ./bar.jsx and ./bar.js
+        resolve: ['.js', '/index.js'] ,
+      }),
+      commonjs({
+        include: 'node_modules/**',
+        exclude: ['node_modules/react', 'node_modules/react-dom'],
+        namedExports: {
+          // left-hand side can be an absolute path, a path
+          // relative to the current directory, or the name
+          // of a module in node_modules
+          'react-sizeme': [ 'withSize' ]
+        }
+      })
+    ]
   },
-  // All the used libs needs to be here
-  external: [
-    'react',
-    'prop-types',
-    'styled-components',
-    'styleguidist',
-    'react-sizeme'
-  ],
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    alias({
-      SRC: resolveApp('src'),  // Will check for ./bar.jsx and ./bar.js
-      resolve: ['.js', '/index.js'] ,
-    }),
-  ]
-}
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/bundle.module.js',
+      format: 'cjs'
+    },
+    // All the used libs needs to be here
+    external: dependencies,
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      alias({
+        SRC: resolveApp('src'),  // Will check for ./bar.jsx and ./bar.js
+        resolve: ['.js', '/index.js'] ,
+      }),
+    ]
+  }
+]
