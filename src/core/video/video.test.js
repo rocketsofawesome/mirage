642d0629cd72d82f1fbad6eccf7396ef6e34b046
jrusso1020 <james.russo@rocketsofawesome.com>
2019-02-04 13:51:38 -0500

    Add fallback images to backgroundVideo component
    Add a fallback image for mobile an desktop backgroundVideo so if he
    video can't load we have a fallback image.

--- src/core/video/video.test.js
+++ src/core/video/video.test.js
@@ -14,28 +14,4 @@ describe('(Component) Video', () => {
     expect(createVideo())
     .toMatchSnapshot()
   })
-
-  describe('video sources', () => {
-    test('setting sources to a string', () => {
-      const srcString = 'https://res.cloudinary.com/roa-canon/video/upload/v1535745827/web/ROA_BrandVideo_20180405.mp4'
-      const videoComponent = createVideo({
-        sources: srcString
-      })
-      expect(videoComponent.find('source').prop('src')).toEqual(srcString)
-    })
-
-    test('setting sources to an array', () => {
-      const srcArray = [
-        'https://res.cloudinary.com/roa-canon/video/upload/v1535745827/web/ROA_BrandVideo_20180405.mp4',
-        'https://res.cloudinary.com/roa-canon/video/upload/v1535745827/web/ROA_BrandVideo_20180405.webm',
-        'https://res.cloudinary.com/roa-canon/video/upload/v1535745827/web/ROA_BrandVideo_20180405.ogv'
-      ]
-      const videoComponent = createVideo({
-        sources: srcArray
-      })
-      videoComponent.find('source').forEach((node, index) => {
-        expect(node.prop('src')).toEqual(srcArray[index])
-      })
-    })
-  })
 })
