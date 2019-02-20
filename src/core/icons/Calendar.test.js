d12d1019f6d86cbf8b15641353037a336427e6d7
Shawn Mealey <mealeyst@gmail.com>
2019-02-20 15:43:06 -0500

    Subscription section styling fixes/new components
    Adding some new SVGs for the SubscriptionSection, also tightened up the
    spacing for various sections. Updated the footer link to be the same
    saize as the headers.

---
+++ src/core/icons/Calendar.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { css } from 'styled-components'
+import 'jest-styled-components'
+
+import Calendar from 'SRC'
+
+const { shallowWithTheme } = global
+
+describe('(Styled Component) Calendar', () => {
+  const createCalendar = (props) => {
+    return shallowWithTheme(<Calendar {...props} />)
+  }
+
+  test('matching the snapshot', () => {
+    expect(createCalendar())
+    .toMatchSnapshot()
+  })
+})
