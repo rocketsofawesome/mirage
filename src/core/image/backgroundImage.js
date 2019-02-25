309515747d41d5e87cc9a6dd2758b883b7bf3bbf
Shawn Mealey <mealeyst@gmail.com>
2019-02-25 17:04:32 -0500

    Adding find polyfill for IE

--- src/core/image/backgroundImage.js
+++ src/core/image/backgroundImage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { withSize } from 'react-sizeme'
+require("jspolyfill-array.prototype.find")
 
 const StyledBackgroundImage = styled.section`
   position: relative;
