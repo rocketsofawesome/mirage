286654db190bec0269ca62aa79680e31daf6511d
Leon and James <pairing@Pairings-iMac.local>
2019-04-26 14:31:50 -0400

    Removing errant console.log

--- src/services/cloudinary.js
+++ src/services/cloudinary.js
@@ -1,8 +1,6 @@
 import { Cloudinary } from 'cloudinary-core'
 const { REACT_APP_CLOUDINARY_DISTRIBUTION } = process.env
 
-console.log(REACT_APP_CLOUDINARY_DISTRIBUTION)
-
 const configureCloudinary = () => {
   const cl = new Cloudinary({
     cloud_name: 'roa-canon',
