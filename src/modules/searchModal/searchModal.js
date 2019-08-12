dbf2d1b3b87d9dc69650f4dcc3e17ece61f6583e
Alex Thomas <alex.thomas@rocketsofawesome.com>
2019-08-12 16:55:21 -0400

    [Finishes #167812370] Update search suggestions

--- src/modules/searchModal/searchModal.js
+++ src/modules/searchModal/searchModal.js
@@ -208,7 +208,7 @@ class BaseSearchModal extends React.Component {
             </SearchClearWrapperDiv>
           </SearchForm>
 
-          <SearchSuggestionsDiv>Find it fast! Try: rainbow, stripe, neon, sequin, comfy…</SearchSuggestionsDiv>
+          <SearchSuggestionsDiv>Find it fast! Try: rainbow, sequin, neon, camo, ombre…</SearchSuggestionsDiv>
           <SearchExitWrapperDiv onClick={() => exitSearch()}>
             <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17">
               <g fill="none" fillRule="evenodd" stroke="#0073D1">
