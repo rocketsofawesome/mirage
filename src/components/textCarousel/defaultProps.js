ccf0c50116f3d181d768230d90b9add28c3679f2
Leonid Shalimov <leonid@rocketsofawesome.com>
2019-08-27 14:16:48 -0400

    Updated quotes

--- src/components/textCarousel/defaultProps.js
+++ src/components/textCarousel/defaultProps.js
@@ -1,19 +1,19 @@
 export default {
   quotes: [
     {
-      id: 'robin',
-      quote: '“I am a fashion stylist which makes me super picky when it comes to dressing my daughter. Not only is ROA is super stylish with amazing prints and fun styles, but the clothes are incredibly soft and well made. I am a HUGE fan. ROA is my go to for my child’s clothing needs.” - Robin',
-      name: 'Robin'
-    },
-    {
       id: 'stephanie',
       quote: '“Great colors, super soft fabric...styles you can’t find anywhere else!” - Stephanie',
       name: 'Stephanie'
     },
     {
-      id: 'Michelle',
-      quote: '“The clothes are very comfortable, colorful, and made for play. My kids love to wear these pieces and go back to them again and again.” - Michelle',
-      name: 'Michelle'
+      id: 'hollie',
+      quote: '“The cutest and most unique clothes! Obsessed!” - Hollie',
+      name: 'Hollie'
+    },
+    {
+      id: 'allison',
+      quote: '“All of the designs are unique and thoughtful. Wish they made mom clothes too!” - Allison',
+      name: 'Allison'
     }
   ],
 }
