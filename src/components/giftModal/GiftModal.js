aa27029c5bdd3cea22b847106cf017847ba7d1ad
Manuel Martinez <vmmf08@gmail.com>
2019-11-11 16:24:54 -0600

    Add final copy and styling to GiftModal

--- src/components/giftModal/GiftModal.js
+++ src/components/giftModal/GiftModal.js
@@ -2,12 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import { P, H3, Close } from 'SRC'
+import { P, H3, Close, InlineImage } from 'SRC'
+import cloudinary from 'SRC/services/cloudinary'
 
 const Container = styled.div`
   border: 3px solid ${props => props.theme.colors.rocketBlue};
-  background-color: ${props => props.theme.colors.lightBlue0};
-  padding: 10px;
+  background-color: white;
+  padding: 10px 20px;
   width: 320px;
   position: absolute;
   top: 50%;
@@ -20,19 +21,34 @@ const Container = styled.div`
 const CloseLink = styled.a`
   float: right;
   cursor: pointer;
+  margin-top: 10px;
 `
 
 const Text = styled(P)`
   margin-bottom: 15px;
 `
 
+const Image = styled(InlineImage)`
+  max-width: 100%;
+  margin-bottom: 15px;
+`
+
+const Title = styled(H3)`
+  margin-top: 30px;
+  margin-bottom: 10px;
+`
+
 const GiftModal = ({ onClose }) => (
   <Container>
     <CloseLink onClick={onClose}>
-      <Close width='1.2rem' />
+      <Close width='1.5rem' />
     </CloseLink>
 
-    <H3>Sending a Gift?</H3>
+    <Title>Sending a Gift?</Title>
+    <Image
+      src={cloudinary.url('web/shop/gifting_pop_up.jpg')}
+      alt='Kid opening a box'
+    />
     <Text>
       At checkout, leave a sweet note that'll show
       on the packing slip. Need an exchange? It's
@@ -42,9 +58,6 @@ const GiftModal = ({ onClose }) => (
     <Text>
       And don't forget, buy 4+ items to get 2% off your
       order!
-    </Text>
-    <Text>
-      * excludes FINAL SALE
     </Text>
   </Container>
 )
