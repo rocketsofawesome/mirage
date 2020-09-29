e3f5c9676088ae41310cd0401076bb1da4a545c6
Manuel Martinez <vmmf08@gmail.com>
2020-09-29 17:29:28 -0500

    Fix checkboxes going missing in different screen sizes
    With this change we'll fix a bug that was causing checkboxes
    to dissappear depending on the screen size.

--- src/components/inputs/Checkbox/Checkbox.js
+++ src/components/inputs/Checkbox/Checkbox.js
@@ -1,66 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { keyframes, css } from 'styled-components'
+import styled from 'styled-components'
 
 import CheckboxSVG from './CheckboxSVG.base'
 import Rect from './Rect.base'
 import Check from './Check.base'
 import Label from 'SRC/core/typography/Label'
 
-const rectFillOut = keyframes`
-  from {
-    fill: rgba(0,115,209, 1.0);
-  }
-  to {
-    fill: rgba(255, 255, 255, 0);
-  }
-`
-
-const rectFillIn = keyframes`
-  from {
-    fill: rgba(255, 255, 255, 0);
-  }
-  to {
-    fill: rgba(0,115,209, 1.0);
-  }
-`
-
-const rectChecked = css`
-  animation: ${rectFillIn} 0.25s linear forwards;
-
-  fill: ${props => props.theme.colors.rocketBlue};
-`
-
-const rectUnchecked = css`
-  animation: ${rectFillOut} 0.25s linear forwards;
-
-  stroke-linecap: round;
-  stroke-dashoffset: 200;
-  fill: none;
-`
-
-const checkDash = keyframes`
-  from {
-    stroke-dashoffset: 200;
-  }
-  to {
-    stroke-dashoffset: 0;
-  }
-`
-
-const checkAnimation = css`
-  animation: ${checkDash} 0.25s linear forwards;
-
-  stroke: ${props => props.theme.colors.white};
-  stroke-dasharray: 200;
-  stroke-dashoffset: 0;
-`
-
 class CheckboxBase extends React.Component {
-  state = {
-    showAnimation: false
-  }
-
   onClick = () => {
     if (this.props.input && this.props.input.onClick) {
       const { input: { onClick } } = this.props
@@ -70,17 +17,18 @@ class CheckboxBase extends React.Component {
 
   render() {
     const { className, input, children, label, width, ...props } = this.props
-    const showAnimation = this.state.showAnimation ? 'showAnimation' : ''
+
     return (
       <Label {...props} lowercase className={className}>
         <input
-          className={showAnimation}
           type='checkbox'
           onClick={this.onClick}
           {...input}
         />
         <CheckboxSVG width={width} />
-        { label ? label : children }
+        <span>
+          { label ? label : children }
+        </span>
       </Label>
     )
   }
@@ -93,43 +41,29 @@ CheckboxBase.propTypes = {
 }
 
 const Checkbox = styled(CheckboxBase)`
-  && {
-    display: flex;
-    align-items: center;
-    font-size: 1.6rem;
-    font-weight: 400;
+  display: flex;
+  align-items: center;
+  font-size: 1.6rem;
+  font-weight: 400;
 
-    input {
-      width: 0;
-      height: 0;
+  ${CheckboxSVG} {
+    flex-shrink: 0;
+  }
 
-      opacity: 0;
-    }
+  input {
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
 
-    input:checked + ${CheckboxSVG} ${Rect} {
-      fill: ${props => props.theme.colors.rocketBlue};
-    }
+  input:checked + ${CheckboxSVG} ${Rect} {
+    fill: ${props => props.theme.colors.rocketBlue};
+  }
 
-    input:checked + ${CheckboxSVG} ${Check} {
-      stroke: ${props => props.theme.colors.white};
-      stroke-dasharray: 200;
-      stroke-dashoffset: 0;
-    }
-
-    .showAnimation:focused + ${CheckboxSVG} ${Rect} {
-      stroke-width: 40;
-    }
-
-    .showAnimation:checked + ${CheckboxSVG} ${Rect} {
-      ${rectChecked}
-    }
-
-    .showAnimation:checked + ${CheckboxSVG} ${Check} {
-      ${checkAnimation}
-    }
-    .showAnimation:not(:checked) + ${CheckboxSVG} ${Rect} {
-      ${rectUnchecked}
-    }
+  input:checked + ${CheckboxSVG} ${Check} {
+    stroke: ${props => props.theme.colors.white};
+    stroke-dasharray: 200;
+    stroke-dashoffset: 0;
   }
 `
 
